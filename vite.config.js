4b4da6efd2bba510217916b21a367030c1e4b554
Alvaro Obyrne <alvarobyrne@gmail.com>
2024-09-19 23:34:56 -0500

    Add process to vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   base: 'https://toplapbogota.github.io/escapar-de-la-pantalla',
-  plugins: [
+  define: {
+    'process.env': {},
+},
+plugins: [
     nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
