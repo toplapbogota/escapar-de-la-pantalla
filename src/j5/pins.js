bab6d1c7ae7cd5cf51ae937d9cbeec282434f1fd
Alvaro Obyrne <alvarobyrne@gmail.com>
2024-09-23 23:47:25 -0500

    Better pin management

--- src/j5/pins.js
+++ src/j5/pins.js
@@ -1,52 +1,43 @@
+import { pinsManager, checkPins } from "./pins/pinsManager";
+
 const pines = [];
 let contadorIndice = 0;
 let pines_global;
 let intervalo = 1000;
 let loopID = -1;
+let loopIds = [];
 
 
 function prender(...pinesID) {
+  checkPins(pinesID, 'prender');
   console.log("pines : ", pinesID);
 
   for (let i = 0; i < pinesID.length; i++) {
-    const pin = pines[pinesID[i]]
-    console.log('pin: ', pin);
-    pin.high();
+    console.log(i);
+    pinsManager.getPinById(pinesID[i]).high();
+    // pines[pinesID[i]].high();
   }
 }
 global.prender = prender
 global.encender = prender
 
 function apagar(...pinesID) {
+  checkPins(pinesID, 'prender');
   console.log("pines : ", pinesID);
 
   for (let i = 0; i < pinesID.length; i++) {
-    pines[pinesID[i]].low();
+    console.log(i);
+    pinsManager.getPinById(pinesID[i]).low();
   }
 }
 
 global.apagar = apagar
 
 function onceArduino(...pinesID) {
-  console.log("pines : ", pinesID);
-
-  pines_global = pinesID;
-
-
-  loopID = setInterval(() => {
-    pines[pinesID[contadorIndice]].high();
-
-    if (contadorIndice > 0) {
-      pines[pinesID[contadorIndice - 1]].low();
-    }
-
-    contadorIndice++;
-
-    if (contadorIndice >= pinesID.length) {
-      clearInterval(loopID)
-    }
-
-  }, intervalo)
+  checkPins(pinesID, 'loopArduino');
+  // let pines = pinsManager.j5Pins
+  loopID = setInterval(() => { doLoopPins(pinesID, true) }, intervalo);
+  loopIds.push(loopID);
 }
 
 global.onceArduino = onceArduino;
@@ -60,8 +51,8 @@ function cambiarIntervalo(argument) {
 global.cambiarIntervalo = cambiarIntervalo;
 
 function loop(...pinesID) {
-  console.log("pines : ", pinesID);
-
+  checkPins(pinesID, 'loopArduino');
+  // let pines = pinsManager.j5Pins
   if (pinesID.length !== 0) {
     pines_global = pinesID;
   }
@@ -70,26 +61,11 @@ function loop(...pinesID) {
   }
 
 
-  loopID = setInterval(() => {
-    pines[pinesID[contadorIndice]].high();
-
-    if (contadorIndice > 0) {
-      pines[pinesID[contadorIndice - 1]].low();
-    }
-    else {
-      pines[pinesID[pinesID.length - 1]].low();
-    }
-
-    contadorIndice++;
-
-    if (contadorIndice >= pinesID.length) {
-      contadorIndice = 0;
-    }
-
-  }, intervalo)
+  loopID = setInterval(() => { doLoopPins(pinesID) }, intervalo)
+  loopIds.push(loopID);
 }
 
-global.loopArduino = loop
+global.loop = loop
 
 function detenerLoop() {
   clearInterval(loopID)
@@ -98,42 +74,93 @@ function detenerLoop() {
 global.detenerLoop = detenerLoop
 
 function apagarTodo() {
-  for (let i = 0; i < 13; i++) {
-    var pin = pines[i];
-    pin.low();
-  }
+  const pins = pinsManager.pins;
+  console.log('apagarTodo : ', pins);
+  pinsManager.stopAll();
+  clearInterval(loopID)
+  loopID = null;
+  loopIds.forEach(x => {
+    clearInterval(x)
+
+  })
+  // apagarServos();
+  apagarPinesSimultaneos();
+}
+
+function apagarPinesSimultaneos() {
   if (pinesSimultaneos) {
     pinesSimultaneos.forEach(function (pin_object) {
       clearTimeout(pin_object.init_id)
       clearTimeout(pin_object.stop_id)
     })
+    pinesSimultaneos.length = 0;
   }
-  apagarServos();
 }
 
 global.apagarTodo = apagarTodo
 
 let pinesSimultaneos;
-function simultaneos(...PinesData) {
+/**
+ * 
+ * @param  {...any} pinesData a list of {pin,start,lapse} objects
+ */
+function simultaneos(...pinesData) {
+  apagarPinesSimultaneos();
   //console.log("PinesData : ",PinesData);
-  pinesSimultaneos = PinesData;
-  for (var i = 0; i < PinesData.length; i++) {
-    PinesData[i].id = i;
-    PinesData[i].ciclo = function () {
+  var pinesIds = pinesData.map(pinDatum => pinDatum.pin);
+  let fivePins = checkPins(pinesIds, 'sameTime');
+  // return;
+  pinesSimultaneos = pinesData;
+  for (var i = 0; i < pinesData.length; i++) {
+    let pinId = pinesData[i].pin;
+    pinesData[i].id = i;
+    pinesData[i].fivePin = pinsManager.getPinById(pinId);
+    pinesData[i].ciclo = function () {
       this.init_id = setTimeout(() => {
         console.log('start', this.pin);
-        pines[this.pin].high();
+        this.fivePin.high();
         this.stop_id = setTimeout(() => {
           console.log('stop', this.pin);
-          pines[this.pin].low();
+          this.fivePin.low();
           pinesSimultaneos[this.id].ciclo();
         },
           this.lapse * 1000)
       }, this.start * 1000)
     }
-    PinesData[i].ciclo();
+    pinesData[i].ciclo();
 
   }
+
 }
 
 global.simultaneos = simultaneos
+
+
+function doLoopPins(pinesID, isOnce = false) {
+
+  console.log('-------------------------------')
+  let currentId = pinesID[contadorIndice];
+  let previousId;
+  let lastId = pinesID[pinesID.length - 1];
+  pinsManager.getPinById(currentId).high();
+  // pines[].high();
+
+  if (contadorIndice > 0) {
+    previousId = pinesID[contadorIndice - 1];
+    pinsManager.getPinById(previousId).low();
+    // pines[previousId].low();
+  }
+  else {
+    pinsManager.getPinById(lastId).low();
+    // pines[lastId].low();   
+  }
+
+  contadorIndice++;
+
+  if (contadorIndice >= pinesID.length) {
+    contadorIndice = 0;
+    if (isOnce) clearInterval(loopID);
+  }
+
+
+}
