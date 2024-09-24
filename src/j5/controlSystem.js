f6ad60041715e9338c56e172004a44c2f4469d4c
Alvaro Obyrne <alvarobyrne@gmail.com>
2024-09-23 23:46:51 -0500

    Format

--- src/j5/controlSystem.js
+++ src/j5/controlSystem.js
@@ -6,15 +6,15 @@ global.five = five
 
 function handleIOReady(io) {
   return () => {
-  console.log('handleIOReady')
-  const board = global.board = new Board({ io, repl: false });
-  board.on('message', (msg) => {
-    console.log("board message: ", msg);
-  });
-  board.on('ready', () => {
-    console.log('johnny five in browser !!!!');
-  });
-  board.on('error', console.error);
+    console.log('handleIOReady')
+    const board = global.board = new Board({ io, repl: false });
+    board.on('message', (msg) => {
+      console.log("board message: ", msg);
+    });
+    board.on('ready', () => {
+      console.log('johnny five in browser !!!!');
+    });
+    board.on('error', console.error);
   }
 }
 
